9881e0258406cfbfaa7d5cc4ab071b0fa49ef6bb
David Ho <hhthanh@gmail.com>
2022-06-20 18:10:03 +0700

    Fixed generate license number

--- simulation/internal/simulation/camera_simulation.go
+++ simulation/internal/simulation/camera_simulation.go
@@ -78,21 +78,21 @@ func generateLicenseNumber() string {
 	kenteken := ""
 	switch typeLN {
 	case 1: // 99-AA-99
-		kenteken = fmt.Sprintf("%2d-%s-%2d", randInt(1, 99), generateRandomCharacters(2), randInt(1, 99))
+		kenteken = fmt.Sprintf("%02d-%s-%02d", randInt(1, 99), generateRandomCharacters(2), randInt(1, 99))
 	case 2: // AA-99-AA
-		kenteken = fmt.Sprintf("%s-%2d-%s", generateRandomCharacters(2), randInt(1, 99), generateRandomCharacters(2))
+		kenteken = fmt.Sprintf("%s-%02d-%s", generateRandomCharacters(2), randInt(1, 99), generateRandomCharacters(2))
 	case 3: // AA-AA-99
-		kenteken = fmt.Sprintf("%s-%s-%2d", generateRandomCharacters(2), generateRandomCharacters(2), randInt(1, 99))
+		kenteken = fmt.Sprintf("%s-%s-%02d", generateRandomCharacters(2), generateRandomCharacters(2), randInt(1, 99))
 	case 4: // 99-AA-AA
-		kenteken = fmt.Sprintf("%2d-%s-%s", randInt(1, 99), generateRandomCharacters(2), generateRandomCharacters(2))
+		kenteken = fmt.Sprintf("%02d-%s-%s", randInt(1, 99), generateRandomCharacters(2), generateRandomCharacters(2))
 	case 5: // 99-AAA-9
-		kenteken = fmt.Sprintf("%2d-%s-%d", randInt(1, 99), generateRandomCharacters(3), randInt(1, 9))
+		kenteken = fmt.Sprintf("%02d-%s-%d", randInt(1, 99), generateRandomCharacters(3), randInt(1, 9))
 	case 6: // 9-AAA-99
-		kenteken = fmt.Sprintf("%d-%s-%2d", randInt(1, 9), generateRandomCharacters(3), randInt(1, 99))
+		kenteken = fmt.Sprintf("%d-%s-%02d", randInt(1, 9), generateRandomCharacters(3), randInt(1, 99))
 	case 7: // AA-999-A
-		kenteken = fmt.Sprintf("%s-%3d-%s", generateRandomCharacters(2), randInt(1, 999), generateRandomCharacters(1))
+		kenteken = fmt.Sprintf("%s-%03d-%s", generateRandomCharacters(2), randInt(1, 999), generateRandomCharacters(1))
 	case 8: // A-999-AA
-		kenteken = fmt.Sprintf("%s-%3d-%s", generateRandomCharacters(1), randInt(1, 999), generateRandomCharacters(2))
+		kenteken = fmt.Sprintf("%s-%03d-%s", generateRandomCharacters(1), randInt(1, 999), generateRandomCharacters(2))
 	}
 
 	return kenteken
