3f8203aa6ed424259b5298048c26e9c5ccdc3cb9
David Ho <hhthanh@gmail.com>
2022-06-21 16:41:29 +0700

    Use configuration for subscription

--- fine-collection-service/internal/fine_collection/http/routes.go
+++ fine-collection-service/internal/fine_collection/http/routes.go
@@ -1,28 +1,11 @@
 package http
 
 import (
-  "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4"
 
-  fc "dapr-workshop-go/fine-collection-service/internal/fine_collection"
-  "net/http"
+	fc "dapr-workshop-go/fine-collection-service/internal/fine_collection"
 )
 
-type Subscription struct {
-  Pubsubname string `json:"pubsubname"`
-  Topic      string `json:"topic"`
-  Route      string `json:"route"`
-}
-
 func MapRoutes(commGroup *echo.Group, h fc.Handlers) {
-  commGroup.POST("collectfine", h.CollectFine())
-
-  commGroup.GET("dapr/subcribe", func(c echo.Context) error {
-    subscription := &Subscription{
-      Pubsubname: "pubsub",
-      Topic:      "speedingviolations",
-      Route:      "/collectfine",
-    }
-
-    return c.JSONPretty(http.StatusOK, subscription, "")
-  })
+	commGroup.POST("collectfine", h.CollectFine())
 }
